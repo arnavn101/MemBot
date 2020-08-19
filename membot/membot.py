866df7abce4eb8a86aac22303233dbfdcba1b4ab
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2020-08-19 17:44:07 -0400

    Initial Commit

---
+++ membot/membot.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import pytorch
+
+#https://www.microsoft.com/en-us/download/details.aspx?id=52375&from=http%3A%2F%2Fresearch.microsoft.com%2Fen-us%2Fdownloads%2F6096d3da-0c3b-42fa-a480-646929aa06f1%2F
+#http://www.cs.cornell.edu/~cristian/Cornell_Movie-Dialogs_Corpus.html
