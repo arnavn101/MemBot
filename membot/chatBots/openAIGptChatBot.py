fe52aaf9220e9d11b563a942da305fcd62f3c6ff
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2020-10-10 18:12:38 -0400

    Minor addition to GTPChatBot

--- membot/chatBots/openAIGptChatBot.py
+++ membot/chatBots/openAIGptChatBot.py
@@ -1,8 +1,24 @@
-from pytorch_pretrained_bert import OpenAIGPTDoubleHeadsModel, OpenAIGPTTokenizer
+import gpt_2_simple as gpt2
 
 
 class ChatBot:
-    def __init__(self):
-        self.this_model = OpenAIGPTDoubleHeadsModel.from_pretrained('openai-gpt')
-        self.this_tokenizer = OpenAIGPTTokenizer.from_pretrained('openai-gpt')
-        raise NotImplementedError
+    def __init__(self, textFilePath='', savedModelPath='', GPT2_model_name='124M', n_epochs=100):
+        self.textFilePath = textFilePath
+        self.savedModelPath = savedModelPath
+        self.GPT2_model_name = GPT2_model_name
+        self.n_epochs = n_epochs
+
+    def downloadModel(self):
+        gpt2.download_gpt2(model_name=self.GPT2_model_name)
+
+    def initiateTraining(self):
+        sessionTf = gpt2.start_tf_sess()
+        gpt2.finetune(sessionTf, self.textFilePath, model_name=self.GPT2_model_name, steps=self.n_epochs)
+        gpt2.generate(sessionTf)
+
+    def returnGeneratedText(self, length=25, temperature=1.0, prefix='I'):
+        sessionTf = gpt2.start_tf_sess()
+        gpt2.load_gpt2(sessionTf, checkpoint_dir=self.savedModelPath)
+        outputText = gpt2.generate(sessionTf, checkpoint_dir=self.savedModelPath, length=length,
+                                   temperature=temperature, prefix=prefix)
+        return outputText
